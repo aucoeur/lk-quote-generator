ae57b979f808568e562d3aa552bc5adeb58704ce
aucoeur <aucoeur.ngo@students.makeschool.com>
2020-03-28 02:20:23 -0700

    Add start tokens

--- markov.py
+++ markov.py
@@ -1,1 +1,9 @@
-from random import choice
+from random import randint, randrange, choice
+from dictogram import Dictogram
+from queue import Queue
+from utils import load_text
+
+# class MarkovChain(dict):
+#     def __init__(self, corpus, order=2):
+#         super().__init__()
+#         self.order = order
